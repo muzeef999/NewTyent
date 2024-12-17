5bfc37f92f6da6aca5f0d612359d80d2d92385eb
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2024-12-17 16:11:00 +0530

    fourth commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     env: {
-      LINKEDIN_CLIENT_ID: process.env.linkedin_client_id,
-      LINKEDIN_CLIENT_SECRET: process.env.linkedin_client_secret,
+      LINKEDIN_CLIENT_ID: process.env.LINKEDIN_CLIENT_ID,
+      LINKEDIN_CLIENT_SECRET: process.env.LINKEDIN_CLIENT_SECRET,
   
-      GOOGLE_CLIENT_ID: process.env.google_client_id,
-      GOOGLE_CLIENT_SECRET: process.env.google_client_secret,
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   
-      FACEBOOK_CLIENT_ID:process.env.facebook_client_id,
-      FACEBOOK_CLIENT_SECRET:process.env.facebook_client_secret
+      FACEBOOK_CLIENT_ID:process.env.FACEBOOK_CLIENT_ID,
+      FACEBOOK_CLIENT_SECRET:process.env.FACEBOOK_CLIENT_SECRET
     },
   };
   
