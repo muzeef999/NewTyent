9cd70fb14c8b79aed20687e6e54cf10f1394cb3a
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-01-21 13:59:27 +0530

    71

--- src/app/(home)/compoents/Slider.jsx
+++ src/app/(home)/compoents/Slider.jsx
@@ -1,17 +1,17 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import homebanner1 from "@/asserts/homeBanners/home_Banner1.jpg";
 import "@/app/style/ResponsiveBanner.css";
 import Banner1_sky from "@/asserts/homeBanners/home_Banner1_Sky.png";
+import NmpDispalayfinal from "../(pages)/(products)/(nmp)/NmpDispalayfinal";
 
 const Slider = () => {
   const [bannerClass, setBannerClass] = useState("");
 
   const bannerClasses = [
     "home_banner_bg_1",
-    "home_banner_bg_2",
-    "home_banner_bg_3",
+    "home_banner_bg_2"
   ];
 
   useEffect(() => {
@@ -23,43 +23,42 @@ const Slider = () => {
   return (
     <div>
       <div className={bannerClass}>
-        <div className="home_banner_bg_1">
-          <div className="sky_Movie" style={{ width: "100%", height: "auto" }}>
-            <Image
-              src={Banner1_sky}
-              alt="Home_banner"
-              layout="responsive"
-              priority
-            />
+        {bannerClass === "home_banner_bg_1" && (
+          <div>
+            <div className="sky_Movie" style={{ width: "100%", height: "auto" }}>
+              <Image
+                src={Banner1_sky}
+                alt="Home_banner"
+                layout="responsive"
+                priority
+              />
+            </div>
+
+            <div className="responsive-container">
+            
+              <NmpDispalayfinal />
+            
           </div>
-          <h2>Don't Just Drink Water - Drink Life!</h2>
-          <p>
-            Boost Immunity, Health, and Vitality <br /> with Tyent Water
-          </p>
-        </div>
-     
+            <h2>Don't Just Drink Water - Drink Life!</h2>
+            <p>
+              Boost Immunity, Health, and Vitality <br /> with Tyent Water
+            </p>
+          </div>
+        )}
 
-      <div className="home_banner_bg_2">
-        <div style={{ width: "100%" }}>
-          <Image
-            src={homebanner1}
-            alt="slider npm top counter"
-            layout="responsive"
-            priority
-          />
-        </div>
-      </div>
+        {bannerClass === "home_banner_bg_2" && (
+          <div>
+            <div style={{ width: "100%" }}>
+              <Image
+                src={homebanner1}
+                alt="slider npm top counter"
+                layout="responsive"
+                priority
+              />
+            </div>
+          </div>
+        )}
 
-      <div className="home_banner_bg_3">
-        <div style={{ width: "100%" }}>
-          <Image
-            src={homebanner1}
-            alt="slider npm top counter"
-            layout="responsive"
-            priority
-          />
-        </div>
-        </div>
       </div>
     </div>
   );
