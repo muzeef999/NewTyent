b5026b519554734c0df1326845b96337d0d9f97c
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-02-02 09:14:07 +0530

    130

--- src/app/(home)/solid-hybrid-mesh-plates/page.js
+++ src/app/(home)/solid-hybrid-mesh-plates/page.js
@@ -75,13 +75,13 @@ const page = () => {
         <div className="section-spacing">
             <Row>
               <Col md={6} className='d-flex justify-content-center align-items-center'>
-              <div>
+              <div data-aos="fade-left">
                 <p>Electrodes or plates are one of the most important components of a water ionizer. These plates are typically made of metals like titanium and are coated with a specialized material like platinum, which helps to facilitate the ionization process. When water passes over the plates, an electric current is passed through the water, creating a separation of positive and negative ions.</p>
                 <p>The design, number, and quality of the plates used in a water ionizer play a crucial role in determining the effectiveness and efficiency of the ionization process. A well-designed plate system with more plates typically offers a higher ionization potential, resulting in the production of more hydrogen-rich alkaline water. Additionally, the quality of the materials used for the plates influences their durability and long-term performance.</p>
                 <p>It’s important to choose a water ionizer with an advanced plate design and high-quality electrodes to ensure optimal hydrogen-rich alkaline water production. This not only enhances health benefits but also provides long-term cost savings by reducing reliance on expensive bottled water.</p>
               </div>
               </Col>
-              <Col md={6}>
+              <Col md={6} data-aos="zoom-in">
                <div style={{width:'100%', height:'auto'}}>
                 <Image src={platesvs} alt="platesvs" layout='responsive' priority />
                </div>
@@ -92,13 +92,13 @@ const page = () => {
             <Row>
             <Col md={6} className='d-flex jjustify-content-center align-items-center'>
             <div className='m-4'>
-              <h1 style={{color:'#008AC7'}}><b>Types of Electrodes/Plates:</b></h1>
+              <h1 data-aos="fade-up" style={{color:'#008AC7'}}><b>Types of Electrodes/Plates:</b></h1>
               <h5>Solid Hybrid mesh plate, Mesh plate & Solid plate</h5>
               </div>
             </Col>
               <Col md={6} className='d-flex justify-content-center align-items-center'>
                 <div>
-                  <p>
+                  <p data-aos="fade-right">
                   Tyent is a recognized leader in water ionizer technology, known for its cutting-edge electrode designs. At the heart of Tyent's innovation are its Solid Hybrid Mesh Plates, Tyent plates are designed based on Faraday's law of equal distribution, using a unique hybrid mesh technology. This innovative design involves transforming a solid plate into a hybrid mesh, combining the strength and durability of solid plates with the efficiency of mesh. The result is an optimal balance for water ionization, delivering superior performance and longevity.
                   </p>
                 </div>
@@ -112,7 +112,7 @@ const page = () => {
          <br/>
          <Row className="d-flex flex-wrap g-3">
                    {RegularReplacement.map((iconItem, index) => (
-                     <Col key={index} md={index < 3 ? 4 : index === 3 ? 7 : 5}>
+                     <Col data-aos="flip-left"  key={index} md={index < 3 ? 4 : index === 3 ? 7 : 5}>
                        <WaterFilterCards
                          title={iconItem.title}
                          content={iconItem.content}
@@ -127,25 +127,26 @@ const page = () => {
        <br/>
        <Row>
        <Col
-            md={6}
+            md={6} data-aos="fade-right"
             className="d-flex justify-content-center alin-items-center"
-            style={{ backgroundColor: "#f2f2f2", color: "#000", aspectRatio:'1/1'}}
+            style={{ backgroundColor: "#f2f2f2", color: "#000",}}
           >
             <div style={{ padding: "60px" }}>
               <h2 style={{ fontWeight: 600 }}>Solid Plates</h2>
-              <p style={{ fontSize: "18px" }}>
+              <p>
               Traditional solid plates have a large surface area and are the oldest and strongest technology in water ionizers. However, they do not ionize the water effectively because the ionization happens unevenly, mainly only at the edges. This results in lower hydrogen production and fewer antioxidants (-ORP). Over time, solid plates also get scale buildup, which affects their performance and requires regular cleaning. They are also bulky and consume more power to give the same ionization, leading to higher energy bills and less efficient water purification compared to newer technologies.
               </p>
             </div>
           </Col>
           <Col
             md={6}
+            data-aos="fade-left"
             className="d-flex justify-content-center alin-items-center"
-            style={{ backgroundColor: "#008AC7", color: "#FFF", aspectRatio:'1/1' }}
+            style={{ backgroundColor: "#008AC7", color: "#FFF", }}
           >
             <div style={{ padding: "60px" }}>
               <h2 style={{ fontWeight: 600 }}>Mesh Plates</h2>
-              <p style={{ fontSize: "18px" }}>
+              <p>
               Mesh plates are generally more efficient for ionization because they have a larger surface area, allowing for better water flow and more effective electrolysis. However, their performance is limited by the unorganized design of the mesh, which can cause uneven ionization and lower hydrogen production (-ORP/antioxidants). While mesh plates are more efficient than solid plates, they don’t provide the same ionization power and production of hydrogen/antioxidants as solid hybrid mesh plates, reducing their effectiveness. Also, some mesh plates may wear out faster, affecting their long-term durability and performance.
               </p>
             </div>
@@ -163,7 +164,7 @@ const page = () => {
                     className="d-flex justify-content-center align-items-center"
                   >
                     <div>
-                      <h3>
+                      <h3  data-aos="fade-down">
                         Molecular Hydrogen -{" "}
                         <span
                           style={{
@@ -175,7 +176,7 @@ const page = () => {
                           An emerging medical Gas
                         </span>
                       </h3>
-                      <p>
+                      <p data-aos="fade-up">
                         Although the research is early, the 1000+ scientific articles
                         suggest that H2 has therapeutic potential in over 170 different
                         human and animal disease models, and essentially every organ of
