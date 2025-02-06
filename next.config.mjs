50f70096da406db760a0c3b7e7d0abac3d32e3a3
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-02-06 08:06:09 +0530

    154

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async headers() {
-      return [
-        {
-          source: "/api/:path*", // Apply headers to all API routes
-          headers: [
-            {
-              key: "Access-Control-Allow-Origin",
-              value: "*", // Allow all domains (change for security)
-            },
-            {
-              key: "Access-Control-Allow-Methods",
-              value: "GET, POST, PUT, DELETE, OPTIONS",
-            },
-            {
-              key: "Access-Control-Allow-Headers",
-              value: "Content-Type, Authorization",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify:true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
