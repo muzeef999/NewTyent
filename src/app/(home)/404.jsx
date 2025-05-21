0d9b2dc4bd9ba8852a78aba749b7763049d160f2
jkmultiMediaInstition <jkmultimediainstitution@gmail.com>
2025-05-21 22:09:07 +0530

    added  sitemap and robot

---
+++ src/app/(home)/404.jsx
@@ -0,0 +1,8 @@
+// pages/404.tsx
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
